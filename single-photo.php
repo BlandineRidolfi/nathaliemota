70ce428ecf57962c0bb5964e8b9ed07a14ee5326
Blandine Ridolfi <blandineridolfi84@gmail.com>
2024-02-28 10:58:40 +0100

    Modification de la single-photo.php et block-photo.php

--- single-photo.php
+++ single-photo.php
@@ -18,7 +18,9 @@ if ($annees_terms && !is_wp_error($annees_terms)) {
     $annee = 'Non défini';
   }
 
+// Récupération des catégories de la photo principale
 $categories = get_the_terms(get_the_ID(), 'categorie');
+$categorie_name = $categories ? $categories[0]->name : '';
 $formats = get_the_terms(get_the_ID(), 'format');
 $FORMATS = $formats ? ucwords($formats[0]->name) : ''; // expression conditionnelle en PHP qui utilise la fonction ucwords pour mettre en majuscule la première lettre de chaque mot dans une chaîne de caractères
 
@@ -37,18 +39,25 @@ $nextThumbnailURL = $nextPost ? get_the_post_thumbnail_url($nextPost->ID, 'thumb
             <div class="photo__container">
                 <!-- J'affiche la photo -->
                 <img src="<?php echo $photoId; ?>" alt="<?php the_title_attribute(); ?>">
+                    <div class="singlePhotoOverlay">
+                        <div class="icon__fullscreen" data-reference="<?php echo esc_attr($reference); ?>" data-full="<?php echo esc_url($photo_url); ?>" data-category="<?php echo esc_attr($categorie_name); ?>">
+                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/fullscreen.svg" alt="Icone fullscreen">
+                        </div>
+                    </div>
             </div>
             
-            <div class="photo__info"> 
-                <div class="photo__title">                
-                    <h2><?php echo get_the_title(); ?></h2>
-                </div>
+            
+            <div class="photo__info">                
+                <h2><?php echo get_the_title(); ?></h2>
+            
                 <div class="photo__taxo">
-                    <p>RÉFÉRENCES: <?php echo esc_html($refUppercase); ?></p>
-                    <p>CATÉGORIE: <?php echo esc_html($categories[0]->name); ?></p>
-                    <p>FORMAT: <?php echo esc_html($FORMATS); ?></p>
-                    <p>TYPE: <?php echo esc_html($type); ?></p>
-                    <p>ANNÉE: <?php echo esc_html($annee); ?></p>
+                    <p>RÉFÉRENCE    : <?php echo esc_html($refUppercase); ?></p>
+                    <p>CATÉGORIE    : <?php echo esc_html($categories[0]->name); ?></p>
+                    <p>FORMAT    : <?php echo esc_html($FORMATS); ?></p>
+                    <p>TYPE    : <?php echo esc_html($type); ?></p>
+                    <p>ANNÉE    : <?php echo esc_html($annee); ?></p>
+
+
                 </div>
             </div>
         </div>
@@ -57,8 +66,7 @@ $nextThumbnailURL = $nextPost ? get_the_post_thumbnail_url($nextPost->ID, 'thumb
     <!-- Partie contact et navigation entre les photos -->
     <div class="contact__container">
         <div class="contact">
-            <p class="interested">Cette photo vous intéresse ?</p>
-            
+            <p class="interested">Cette photo vous intéresse ?</p>            
             <!-- Bouton CTA avec référence -->
             <button id="contact__button" data-reference="<?php esc_attr($reference); ?>">Contact</button>
         </div>
@@ -79,51 +87,58 @@ $nextThumbnailURL = $nextPost ? get_the_post_thumbnail_url($nextPost->ID, 'thumb
                 <img class="arrow arrow__right" src="<?php echo get_theme_file_uri() . '/assets/images/arrow_right.png'; ?>" alt="Photo suivante" data-thumbnail-url="<?php echo $nextThumbnailURL; ?>" data-target-url="<?php echo esc_url(get_permalink($nextPost->ID)); ?>">
             <?php endif; ?>
         </div>
-
     </div>
   </div>
 </section>
 
 <section>
-  <div class="title__suggestion">
-    <h3>VOUS AIMEREZ AUSSI</h3>
-  </div>
-  <div class="photo__similar">
-    <?php
-    // Récupération des catégories de la photo principale
-    $categories = get_the_terms(get_the_ID(), 'categorie');
-    // Arguments de la requête pour récupérer les photos similaires
-    $args = array(
-      'post_type' => 'photo',
-      'posts_per_page' => 2,
-      'post__not_in' => array(get_the_ID()),
-      'tax_query' => array(
-        array(
-          'taxonomy' => 'categorie',
-          'field' => 'id',
-          'terms' => $categories ? wp_list_pluck($categories, 'term_id') : array(),
-        ),
-      ),
-    );
-    // Exécution de la requête WP_Query avec les arguments définis
-    $query = new WP_Query($args);
-    // Boucle à travers les photos similaires
-    while ($query->have_posts()) :
-      $query->the_post();
-      // Récupération de l'ID de la photo et de la référence
-      $photoId = get_field('photo');
-      $reference = get_field('reference');
-      $refUppercase = strtoupper($reference);
-      // Affiche le bloc de photo en utilisant un template part (partie de modèle)
-      get_template_part('template-parts/block-photo');
-    endwhile;
-    // Affiche un message si aucune photo similaire n'est trouvée
-    if (!$query->have_posts()) :
-      echo '<p class="photoNotFound">Aucune photo similaire n\'a été trouvée pour la catégorie.</p>';
-    endif;
-    // Réinitialisation des données de post après la boucle de requête
-    wp_reset_postdata();
+    <div class="title__suggestion">
+        <h3>VOUS AIMEREZ AUSSI</h3>
+    </div>
+
+    <div class="photo__similar">
+        <?php
+            $categories = get_the_terms(get_the_ID(), 'categorie'); // Récupère les catégories de la photo principale
+        // Arguments de la requête pour récupérer les photos similaires
+        $args = array(
+            'post_type' => 'photo',
+            'posts_per_page' => 2,
+            'post__not_in' => array(get_the_ID()),
+            'orderby'=> 'rand',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'categorie',
+                    'field' => 'id',
+                    'terms' => $categories ? wp_list_pluck($categories, 'term_id') : array(),
+                ),
+            ),
+        );
+        // Exécution de la requête WP_Query avec les arguments définis
+        $query = new WP_Query($args);
+
+        // Boucle à travers les photos similaires
+        if ($query->have_posts()) :
+            while ($query->have_posts()) : $query->the_post();        
+            // Récupération de l'ID de la photo et de la référence
+            $photoId = get_field('photo');
+            $reference = get_field('reference');
+            $refUppercase = strtoupper($reference);
+
+            // Affiche le bloc de photo en utilisant un template part (partie de modèle)
+            get_template_part('template-parts/block-photo');
+            ?>
+       
+       <?php
+        endwhile;
+      else :
+        // Afficher un message si aucune photo similaire n'est trouvée avec la classe photoNotFound
+        echo '<p class="photoNotFound">Pas de photo similaire trouvée pour la catégorie.</p>';
+      endif;
+      wp_reset_postdata();
     ?>
-  </div>
+        
+       
+    </div>
 </section>
+
 <?php get_footer(); ?>
--- template-parts/block-photo.php
+++ template-parts/block-photo.php
@@ -1,7 +1,7 @@
 <?php
 
 // Je récupère la photo
-$id_photo = get_field('photo');
+$photoId = get_the_post_thumbnail_url(null, "large");
 // Je récupère le titre de la photo
 $title_photo = get_the_title();
 // Je récupère l'url du post
@@ -10,13 +10,13 @@ $url_post = get_permalink();
 $reference = get_field('reference');
 // Je récupère les catégories de la photo
 $categories = get_the_terms(get_the_ID(), 'categorie');
-$categorie_name = $categorie[0]->name;
+$categorie = $categories[0]->name;
 
 ?>
 
 <div class="blockPhotoRelative">
     <!-- J'affiche la photo en incluant son URL et son alt -->
-    <img src="<?php echo esc_url($id_photo); ?>" alt="<?php the_title_attribute(); ?>">
+    <img src="<?php echo esc_url($photoId); ?>" alt="<?php the_title_attribute(); ?>">
 
     <div class="overlay">
 
@@ -24,20 +24,16 @@ $categorie_name = $categorie[0]->name;
         <h2><?php echo esc_html($title_photo); ?></h2>
 
         <!-- J'affiche la catégorie -->
-        <h3><?php echo esc_html($categorie_name); ?></h3>
+        <h3><?php echo esc_html($categorie); ?></h3>
 
         <!-- J'ajoute l'icône pour voir la photo en entier -->
         <div class="eye-icon">
-            <a href="<?php echo esc_url($url_post); ?>">
+            <a href="<?php echo esc_url(get_permalink()); ?>">
                 <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/icon_eye.svg" alt="voir la photo">
             </a>
         </div>
-
-        <!-- Je vérifie si la référence est définie avant d'afficher l'icône fullscreen -->
-        <?php if ($reference) : ?>
-            <div class="icon-fullscreen" data-full="<?php echo esc_attr($id_photo); ?>" data-category="<?php echo esc_attr($categorie_name); ?>" data-reference="<?php echo esc_attr($reference); ?>">
-                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/fullscreen.svg" alt="Icone fullscreen">
-            </div>
-        <?php endif; ?>
+        <div class="icon-fullscreen" data-full="<?php echo esc_attr($id_photo); ?>" data-category="<?php echo esc_attr($categorie); ?>" data-reference="<?php echo esc_attr($reference); ?>">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/fullscreen.svg" alt="Icone fullscreen">
+        </div>
     </div>
 </div>
